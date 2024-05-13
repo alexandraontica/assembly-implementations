95f8ec2ca3e7f69af9ef14b226a50425575be6c3
alexandraontica <alexandra.ontica@gmail.com>
2024-05-14 00:40:28 +0300

    completed task 1

--- paranthesinator.asm
+++ paranthesinator.asm
@@ -12,69 +12,74 @@ check_parantheses:
 
     mov esi, [ebp + 8]
 
-    xor eax, eax  ; assume the parantheses are close correctly
-                  ; if not, return 1
+    ; assume the parantheses are closed correctly
+    xor eax, eax
 
-    xor ecx, ecx
-    xor edx, edx  ; if I add something to the stack, edx becomes 1
+    xor edx, edx
+    ; if I add something to the stack, edx becomes 1
+
 check_each_character:
-    cmp byte [esi + ecx], 0
+    cmp byte [esi], 0
     je end
 
-    cmp byte [esi + ecx], '('
+    ; add the open parantheses to the stack
+    cmp byte [esi], '('
     je add_to_stack
 
-    cmp byte [esi + ecx], '['
+    cmp byte [esi], '['
     je add_to_stack
 
-    cmp byte [esi + ecx], '{'
+    cmp byte [esi], '{'
     je add_to_stack
+
+    cmp byte [esi], ')'
+    je check_stack
+
+    cmp byte [esi], ']'
+    je check_stack
+
+    cmp byte [esi], '}'
+    je check_stack
 
 add_to_stack:
-    movzx edi, byte [esi + ecx]
-    push edi
+    push esi
     mov edx, 1
     jmp next_iteration
 
-    cmp byte [esi + ecx], ')'
-    je check_stack
-
-    cmp byte [esi + ecx], ']'
-    je check_stack
-
-    cmp byte [esi + ecx], '}'
-    je check_stack
-
 check_stack:
+    ; check if I added something to the stack
     cmp edx, 0
-    je fount_mismatch
+    je found_mismatch
 
+    ; when a closing parantheses is met,
+    ; check if its "match" was met right before
+    ; (check if the "match" is the last charcter added to the stack)
     pop edi
-    cmp byte [esi + ecx], ')'
+    cmp byte [esi], ')'
     jne parantheses2
 
     cmp byte [edi], '('
-    jne fount_mismatch
+    jne found_mismatch
 
 parantheses2:
-    cmp byte [esi + ecx], ']'
+    cmp byte [esi], ']'
     jne parantheses3
 
     cmp byte [edi], '['
-    jne fount_mismatch
+    jne found_mismatch
 
 parantheses3:
-    cmp byte [esi + ecx], '}'
+    cmp byte [esi], '}'
     jne next_iteration
 
     cmp byte [edi], '{'
-    jne fount_mismatch
+    jne found_mismatch
 
 next_iteration:
-    inc ecx
+    inc esi
     jmp check_each_character
 
-fount_mismatch:
+found_mismatch:
     mov eax, 1
 
 end:
