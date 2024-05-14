53bba50f4e9eacd00260882e1e5e1205ae2c6c5c
alexandraontica <alexandra.ontica@gmail.com>
2024-05-14 13:48:44 +0300

    seg fault... again...

---
+++ subtask_2.asm
@@ -0,0 +1,61 @@
+; subtask 2 - bsearch
+
+section .text
+    global binary_search
+    ;; no extern functions allowed
+
+binary_search:
+    ;; create the new stack frame
+    enter 0, 0
+
+    ;; save the preserved registers
+    pusha
+
+    ;; recursive bsearch implementation goes here
+    ; fastcall:
+    ; ecx = buff
+    ; edx = needle
+    mov ecx, [ecx]
+    mov eax, [ebp + 8]
+    mov ebx, [ebp + 12]
+    cmp ebx, eax
+    jl not_found
+
+    ; (end - start) / 2
+    sub ebx, eax
+    shr ebx, 1
+
+    ; mid
+    add eax, ebx
+
+    cmp [ecx + eax * 4], edx
+    jg check_left
+    jl check_right
+
+    ; needle == buff[mid]
+    jmp end_bsearch
+
+check_left:
+    dec eax
+    mov esi, [ebp + 8]
+    push eax
+    push esi
+    call binary_search
+    add esp, 8
+
+check_right:
+    inc eax
+    mov esi, [ebp + 12]
+    push esi
+    push eax
+    call binary_search
+    add esp, 8
+
+not_found:
+    mov eax, -1
+
+end_bsearch:
+    ;; restore the preserved registers
+    popa
+    leave
+    ret
